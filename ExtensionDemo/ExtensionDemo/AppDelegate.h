fce2ceede0751b8217e68e5a248e4f4e016c9806
JaySonGD <czljcb@163.com>
2016-02-23 09:02:19 +0800

    code init

---
+++ ExtensionDemo/ExtensionDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ExtensionDemo
+//
+//  Created by czljcb on 16/2/23.
+//  Copyright © 2016年 czljcb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ExtensionDemo/ExtensionDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ExtensionDemo
+//
+//  Created by czljcb on 16/2/23.
+//  Copyright © 2016年 czljcb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
